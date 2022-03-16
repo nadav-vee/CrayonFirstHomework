de83eb20bf5163fd6e0101c047cf882b1c1fbbe5
nadav <nadav@HARPER>
2022-03-16 17:43:21 +0200

    Add project files.

---
+++ CrayonFirstHomework/ScreenREC.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+using System.IO;
+
+using Accord.Video.FFMPEG;
+
+namespace CrayonFirstHomework
+{
+    class ScreenREC
+    {
+        // video variables:
+        private Rectangle bounds;
+        private string outputPath = "";
+        private string tempPath = "";
+        private int fileCount = 1;
+        private List<string> inputImageSequence = new List<string>();
+
+        // File variables:
+        private string audioName = "mic.wav";
+        private string videoName = "video.mp4";
+        private string fileName = "FinalVideo.mp4";
+
+        // Time variable:
+        Stopwatch watch = new Stopwatch();
+
+        // Audio variables:
+        public static class NativeMethods
+        {
+            [DllImport("winmm.dll", EntryPoint = "mciSendStringA", CharSet = CharSet.Ansi, SetLastError = true, ExactSpelling = true)]
+            public static extern int record(string lpstrCommand, string lpstrReturnString, int uReturnLength, int hwndCallback);
+        }
+        public ScreenREC(Rectangle b, string outPath)
+        {
+            CreateTempFolder("tempScreenShots");
+
+            bounds = b;
+            outputPath = outPath;
+        }
+
+        private void CreateTempFolder(string name)
+        {
+            if (Directory.Exists("D://"))
+            {
+                string pathName = $"D://{name}";
+                Directory.CreateDirectory(pathName);
+                tempPath = pathName;
+            }
+            else
+            {
+                string pathName = $"C://{name}";
+                Directory.CreateDirectory(pathName);
+                tempPath = pathName;
+            }
+        }
+
+        private void DeletePath(string tarDir)
+        {
+            string[] files = Directory.GetFiles(tarDir);
+            string[] dirs = Directory.GetDirectories(tarDir);
+
+            foreach (string file in files)
+            {
+                File.SetAttributes(file, FileAttributes.Normal);
+                File.Delete(file);
+            }
+
+            foreach (string dir in dirs)
+            {
+                DeletePath(dir);
+            }
+
+            Directory.Delete(tarDir, false);
+        }
+
+        private void DeleteFilesExcept(string targetFile, string excFile)
+        {
+            string[] files = Directory.GetFiles(targetFile);
+
+            foreach (string file in files)
+            {
+                if (file != excFile)
+                {
+                    File.SetAttributes(file, FileAttributes.Normal);
+                    File.Delete(file);
+                }
+            }
+
+        }
+
+        public void CleanUp()
+        {
+            if (Directory.Exists(tempPath))
+            {
+                DeletePath(tempPath);
+            }
+        }
+
+        public string GetElapsed()
+        {
+            return string.Format(
+                "{0:D2}:{1:D2}:{2:D2}",
+                watch.Elapsed.Hours,
+                watch.Elapsed.Minutes,
+                watch.Elapsed.Seconds);
+        }
+
+        public void RecordVideo()
+        {
+            watch.Start();
+
+            using (Bitmap bitmap = new Bitmap(bounds.Width, bounds.Height))
+            {
+                using (Graphics g = Graphics.FromImage(bitmap))
+                {
+                    g.CopyFromScreen(new Point(bounds.Left, bounds.Top), Point.Empty, bounds.Size);
+                }
+                string name = tempPath + "//Screenshot-" + fileCount + ".png";
+                bitmap.Save(name, ImageFormat.Png);
+                inputImageSequence.Add(name);
+                fileCount++;
+            }
+        }
+
+        public void RecordAudio()
+        {
+            NativeMethods.record("open new Type waveaudio Ailias recsound", "", 0, 0);
+            NativeMethods.record("record recsound", "", 0, 0);
+        }
+
+        private void SaveVideo(int width, int height, int framRate)
+        {
+            using (VideoFileWriter vfWriter = new VideoFileWriter())
+            {
+                vfWriter.Open(outputPath + "//" + videoName, width, height, framRate, VideoCodec.MPEG4);
+
+                foreach (string imageLoc in inputImageSequence)
+                {
+                    using (Bitmap imageFrame = System.Drawing.Image.FromFile(imageLoc) as Bitmap)
+                    {
+                        vfWriter.WriteVideoFrame(imageFrame);
+                    }
+                }
+
+                vfWriter.Close();
+            }
+        }
+
+        private void SaveAudio()
+        {
+            string audioPath = "save recsound " + outputPath + "//" + audioName;
+            NativeMethods.record(audioPath, "", 0, 0);
+            NativeMethods.record("close recsound", "", 0, 0);
+        }
+
+        private void CombineVideoAndAudio(string video, string audio)
+        {
+
+        }
+    }
+}
